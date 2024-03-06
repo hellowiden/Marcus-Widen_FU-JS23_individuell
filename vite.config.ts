273b81d8b7c65cc3fcb93b19aba7bb99aa2241ae
Marcus Widen <hellowiden@gmail.com>
2024-03-06 22:15:29 +0100

    setBase

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/Marcus-Widen_FU-JS23_individuell',
 })
